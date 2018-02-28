e4bf573ae3656f6b55429dc2ef67a55a09dac733
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2018-02-28 15:41:11 +0000

    Setup aula4

---
+++ aula04-naive-queries/src/main/java/util/HttpRequest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2017, Miguel Gamboa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Miguel Gamboa
+ *         created on 08-03-2017
+ */
+public class HttpRequest implements IRequest {
+    @Override
+    public Iterable<String> getContent(String path) {
+        List<String> res = new ArrayList<>();
+        try (InputStream in = new URL(path).openStream()) {
+            /*
+             * Consumir o Inputstream e adicionar dados ao res
+             */
+            try(BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    res.add(line);
+                }
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+        return res;
+    }
+}
---
+++ aula04-naive-queries/src/test/java/QueriesTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import util.FileRequest;
+import util.HttpRequest;
+import weather.WeatherWebApi;
+import weather.dto.WeatherInfo;
+
+import java.time.LocalDate;
+
+import static java.time.LocalDate.of;
+import static org.junit.Assert.*;
+
+public class QueriesTest {
+    @Test public void testSomeThing() {
+        // WeatherWebApi weather = new WeatherWebApi(new HttpRequest());
+        WeatherWebApi weather = new WeatherWebApi(new FileRequest());
+        Iterable<WeatherInfo> past = weather
+                .pastWeather(41.15, -8.6167, of(2017, 2, 1), of(2017, 4, 30));
+        past.forEach(System.out::println);
+
+    }
+}
