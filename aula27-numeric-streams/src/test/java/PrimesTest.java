a71cd7e658809a47fb9807b0a7f5b898254b17c9
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2018-05-09 17:43:21 +0100

    PrimesTest

--- aula27-numeric-streams/src/test/java/PrimesTest.java
+++ aula27-numeric-streams/src/test/java/PrimesTest.java
@@ -1,2 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Supplier;
+
 public class PrimesTest {
+
+    @Test
+    public void testPrimes() {
+    }
+
+    /**
+     * !!!! Do not Use for reliable performance tests.
+     * For accurate micro benchmarking you must use JMH!!!!
+     */
+    private static <T> T measurePerformance(Supplier<T> action) {
+        T val = null;
+        long min = Long.MAX_VALUE;
+        for (int i = 0; i < 100; i++) {
+            long init = System.nanoTime();
+            val = action.get();
+            long dur = System.nanoTime() - init;
+            if(dur < min) min = dur;
+        }
+        min = min / 1000;
+        System.out.println("Duration = " + min + "us");
+        return val;
+    }
 }
